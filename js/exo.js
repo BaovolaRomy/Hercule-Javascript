b317270ad64855de074a5a056f5b5e38af5bbf89
BaovolaRomy <baoromy@icloud.com>
2022-10-20 10:22:05 +0200

    etapes-jusqu'a-7

--- js/exo.js
+++ js/exo.js
@@ -5,7 +5,7 @@ const hercule = {
     age : 35,
     department : 75,
     arm : 60.5,
-    inRelationship : 'oui',
+    inRelationship : true,
 
 //ETAPE 3
     amis : [
@@ -13,9 +13,46 @@ const hercule = {
         'Junon ',
         'Alcmène ',
         'Déjanire ',
-    ]
+    ],
+
 }
+
 base.fillProfil(hercule); 
 base.printFriends(hercule.amis);
 base.setBestFriend(hercule.amis[0]);
 
+
+// ETAPE 4
+const h1 = document.createElement('h1');
+h1.classList.add('banner__title');
+h1.textContent = 'Vous consultez le profil de Hercule';
+const headerBanner = document.getElementById('header-banner');
+headerBanner.appendChild(h1);
+
+// ETAPE 5
+let i=0;
+for (i; i<11; i++){
+   base.displayWork(i);
+}
+base.displayWork(0);
+
+// ETAPE 6
+const disponibilite = true;
+const availability = document.getElementById('availability');
+if(base.getHour() >= 8 && base.getHour()<20){
+    disponibilite;
+    availability.innerHTML = 'Disponible';
+}else{
+    availability;
+    availability.classList.add('off');
+}
+
+// ETAPE 7
+function getRandomPseudo (prenom='', department=0){
+    return(`${prenom}-du-${department}`);
+}
+const pseudo = getRandomPseudo('Hercule',hercule.department);
+const profilName = document.getElementById('profil-name');
+profilName.textContent = `${pseudo}`;
+
+// ETAPE 8
