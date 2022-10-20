34132a7f3e4f2521454ae3253421d2f54997c905
BaovolaRomy <baoromy@icloud.com>
2022-10-20 09:23:34 +0200

    3-premieres-etapes

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,21 @@
-console.log('test 1...2...1...2');
+//ETAPE 2
+const hercule = {
+    name : 'Hercule',
+    job : 'Demi-Dieu',
+    age : 35,
+    department : 75,
+    arm : 60.5,
+    inRelationship : 'oui',
+
+//ETAPE 3
+    amis : [
+        'Jupiter ',
+        'Junon ',
+        'Alcmène ',
+        'Déjanire ',
+    ]
+}
+base.fillProfil(hercule); 
+base.printFriends(hercule.amis);
+base.setBestFriend(hercule.amis[0]);
+
